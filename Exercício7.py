3b9c4810673ee38f595a9d59f925ed78fb792a98
joaovictorVsales <163188697+joaovictorVsales@users.noreply.github.com>
2024-08-16 22:08:10 -0300

    Add files via upload

---
+++ Exercício7.py
@@ -0,0 +1,20 @@
+C= int(input('Entre com um código de acesso a um curso: '))
+
+if(C==1):
+    print('Engenharia')
+
+if(C==2):
+    print('Edificações')
+
+if(C==3):
+    print('Sistemas elétricos')
+
+if(C==4):
+    print('Turismo')
+
+if(C==5):
+    print('Análise de sistemas')
+
+if(C>=6):
+    print('Código inválido')
+

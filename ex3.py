8ca5974e69ece2f7e526ffd75f21ad276c7378ee
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2024-09-18 11:32:06 -0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,7 @@
+n1= float(input('Digite a sua nota: '))
+n2= float(input('Digite a sua nota: '))
+n3= float(input('Digite a sua nota: '))
+n4= float(input('Digite a sua nota: '))
+media= (n1+n2+n3+n4)/4
+
+print(f'Sua média é igual a {media:.0f}')
